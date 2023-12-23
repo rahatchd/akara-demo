c13d9cdeb9d584b7090ba2173fd9e063bbdab624
Rahat Dhande <rahatchd@gmail.com>
2023-12-23 18:37:49 +0000

    use secure url

--- js/Store.js
+++ js/Store.js
@@ -1,4 +1,4 @@
-const API_URL = 'http://rahatchd.github.io/akara-demo/data/exercises.json';
+const API_URL = 'https://rahatchd.github.io/akara-demo/data/exercises.json';
 
 export class Store {
   static formatData(datum) {
